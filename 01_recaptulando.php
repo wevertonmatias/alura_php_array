7a91491436fc223f88d6909038b92ced4ad35d94
Weverton Matias <weverton-matias@hotmail.com>
2022-12-19 09:02:47 -0300

    Relembrando foreach.

--- 01_recaptulando.php
+++ 01_recaptulando.php
@@ -5,4 +5,15 @@ $teste = [
     1.9 => 'tres'
 ];
 
-var_dump($teste);
+$transportes =[
+    'carro' => 'azul',
+    'aviao' => 'verde',
+    'moto' => 'vermelha',
+    'barco' => 'branca'
+    ];
+
+foreach($transportes as $chave => $valor){
+    echo "Tipo :{$chave} - Cor: {$valor}". PHP_EOL;
+}
+echo "Total de itens ".count($transportes). PHP_EOL;
+//var_dump($teste);
