3353a0a6d1173316f4037759059cff37e2f10a21
Weverton Matias <weverton-matias@hotmail.com>
2022-12-23 15:09:16 -0300

    Array verificações de valores.

--- 03_verificacoes.php
+++ 03_verificacoes.php
@@ -1,6 +1,6 @@
 <?php
 $notas = [
-    'Ana' => 8, 'Paulo' => 10, 'Carla' => 6, 'Fabio' =>9
+    'Ana' => 8, 'Paulo' => 10, 'Carla' => 6, 'Fabio' => null
 ];
 
 echo '**********Verificando se é array **********'.PHP_EOL;
@@ -9,3 +9,8 @@ var_dump(is_array($notas));
 
 //echo '***Verificando se um array tem índices numéricos e estão em ordem crescente***'.PHP_EOL;
 //var_dump(array_is_list()); disponível apenas em php 8.2
+echo '******* Verificando se key existe *******'.PHP_EOL;
+var_dump(array_key_exists('Paulo', $notas));
+var_dump(array_key_exists('Paula', $notas));
+var_dump(isset($notas['Fabio']));
+var_dump(isset($notas['Paulo']));
